308f32000a6224c861440000500524bb2c426390
jairoMolina9 <molinaandres@gmail.com>
2023-01-26 23:43:40 -0500

    Test 1.26.23

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,25 @@
 import Event from "./EventMain";
 import './App.css';
+import { useSearchParams } from "react-router-dom";
 
+function Display() {
+  const [searchParams] = useSearchParams();
+  const eventiD = searchParams.get('eventid');
+  
+  if(eventiD) {
+    return <Event eventid={eventiD}> </Event>
+  } else {
+    return <h1>SCAN A QR CODE</h1>
+  }
+
+}
 
 function App() {
 
+
   return (
     <div className="App">
-      <Event> </Event>
+      {Display()}
     </div>
   );
 }
--- my-app/src/EventMain.js
+++ my-app/src/EventMain.js
@@ -1,19 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
 
-export default function EventMain() {
+export default function EventMain(props) {
     const [eventName, setEventName] = useState("");
     const [eventDate, setEventDate] = useState({});
     const [eventAddress, setEventAddress] = useState({});
     const [eventVendors, setEventVendors] = useState([]);
 
-    const [searchParams] = useSearchParams();
-    const eventid = searchParams.get('eventid');
-    
     useEffect(() => {
 
         const fetchData = async() => {
-            await fetch("http://localhost:3002/?eventid="+eventid)
+            await fetch("http://localhost:3002/?eventid="+props.eventid)
             .then((response) => response.json())
             .then((data) => {
                 let eventData = data.eventData[0];
@@ -23,7 +19,7 @@ export default function EventMain() {
                 setEventVendors(eventData.Vendors);
             });
         }
-
+        
         fetchData();
 
     }, []);
@@ -31,11 +27,20 @@ export default function EventMain() {
 
     return(
         <div className="EventMain">
-            <h1>{eventName}</h1>
+            <h1 id="title">{eventName}</h1>
             <div>
-                {eventVendors.map((vendor) =>
-                    <li key={vendor.Name}>{vendor.Name}</li>
-                )}
+                <ul>               
+                    {eventVendors.map((vendor) =>
+                        <>
+                        <li key={vendor.Name}>{vendor.Name}</li>
+                            {/* <ul>
+                                {vendor.Menu.map((menuItem) =>
+                                <li key={menuItem.Name}>{menuItem.Name} "/" {menuItem.Price}</li>
+                                )}
+                            </ul> */}
+                        </>
+                    )}
+                </ul>
             </div>
         </div>
     );
